b7730b648f1e6a1b4f0c8470b5949bc7c8e9a51d
Doug Shamoo <doug.shamoo@gmail.com>
2015-08-24 12:08:04 -0700

    Implemented ForagerBee class

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
 var ForagerBee = function() {
+  this = new Bee();
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+}
 
