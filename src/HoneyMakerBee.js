ee05c76aeccd7d82e0a2a4a356f84c6ab6bca1a5
Doug Shamoo <doug.shamoo@gmail.com>
2015-08-24 12:07:39 -0700

    Implemented HoneyMakerBee class

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,14 @@
 var HoneyMakerBee = function() {
+  this = new Bee();
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
