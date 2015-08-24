e9a68276691220f685c07c309c567212d2ec8d76
Doug Shamoo <doug.shamoo@gmail.com>
2015-08-24 15:12:06 -0700

    Re-implemented all classes correctly using pseudoclassical subclassing, passes all tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,12 +1,14 @@
 var ForagerBee = function() {
-  this = new Bee();
+  Bee.call(this);
   this.age = 10;
   this.job = 'find pollen';
   this.canFly = true;
   this.treasureChest = [];
 };
 
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
 ForagerBee.prototype.forage = function(treasure) {
   this.treasureChest.push(treasure);
 }
-
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,9 +1,12 @@
 var HoneyMakerBee = function() {
-  this = new Bee();
+  Bee.call(this);
   this.age = 10;
   this.job = 'make honey';
   this.honeyPot = 0;
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
 
 HoneyMakerBee.prototype.makeHoney = function() {
   this.honeyPot++;
